69853a4292d9e22505030f8ece4f380a5f421bfd
Sonif <sonifdev@gmail.com>
2025-05-26 10:53:37 +0700

    ignore config

--- application/config/config.sample.php
+++ application/config/config.sample.php
@@ -23,10 +23,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
-$fo = str_replace("index.php","", $_SERVER['SCRIPT_NAME']);
-$config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] .':'.$_SERVER['SERVER_PORT']. $fo;
-// $config['base_url'] = 'http://ujol.test';
+// $http = 'http' . ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == 'on') ? 's' : '') . '://';
+// $fo = str_replace("index.php","", $_SERVER['SCRIPT_NAME']);
+// $config['base_url']    = "$http" . $_SERVER['SERVER_NAME'] .':'.$_SERVER['SERVER_PORT']. $fo;
+$config['base_url'] = 'http://domain.test';
 
 /*
 |--------------------------------------------------------------------------
