e3ff506af88913948a2b97b3869b191b04e12530
Sonif <sonifdev@gmail.com>
2025-08-13 21:17:07 +0700

    add cors on auth controller api

--- src/application/modules/api/controllers/Auth.php
+++ src/application/modules/api/controllers/Auth.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 class Auth extends CI_Controller {
 
   public function __construct()
