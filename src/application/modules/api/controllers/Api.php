20c5b7f5dab2dc148e9e943a56ea1915d63a7ab4
Adi Falih <spidey354@gmail.com>
2025-09-03 10:34:56 +0700

    Adjust created_at timestamp to account for timezone offset

--- src/application/modules/api/controllers/Api.php
+++ src/application/modules/api/controllers/Api.php
@@ -245,7 +245,10 @@ class Api extends REST_Controller {
                 }
             }
             // $this->db->trans_start();
-            $data['created_at'] = date("Y-m-d H:i:s");
+            $now = new DateTime();
+            $now->modify('+7 hours');
+            $data['created_at'] = $now->format('Y-m-d H:i:s');
+            // $data['created_at'] = date("Y-m-d H:i:s");
             if (empty($id)) {
                 $data['created_by'] = $this->data['user']->id;
                  if($this->data['user']->usergroup_id !=1){
