391d25c3fc533c8f1118b78a448d7c1d79561db9
Adi Falih <spidey354@gmail.com>
2025-10-22 11:10:14 +0700

    add button

--- src/assets/js/formx/general_utils.js
+++ src/assets/js/formx/general_utils.js
@@ -5,3 +5,24 @@ function selfEmployeeId(formId) {
   $(formId+" select[name=employee_id]").attr("readonly", "readonly");
   $(formId+" select[name=employee_id]").val(employeeid);
 }
+
+$(".jq-terima-stock").on('click', function() {
+    var stock_id = $(this).attr('lang');
+    stock_clear(stock_id);
+})
+
+
+function stock_clear(stock_id) {
+    alert("LANG: " + stock_id);
+    // $.post( baseUrl+"/transaksi/infometeran/",
+    //   {
+    //     customer_id: customer,
+    //     year:year,
+    //     month: month
+    //   }
+    // , function( data ) {
+    //   var newinfo = data.value;
+    //   $("#input_form_26 input[name=transaksi_count_now]").val(newinfo);
+    // });
+  
+}
